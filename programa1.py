15fcafaaf3159c4e6bd8c8fa6e7b8e80798e5981
Rodolfo Pardo <rodypardo@gmail.com>
2019-06-21 18:30:53 -0300

    Nuevo programa

---
+++ programa1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 21 18:10:09 2019
+
+@author: rodolfopardo
+"""
+
+#En esta vamos a focalizar una nueva jornada laboral
+
+print(len("Rodolfo"))
+print(len("Cecilia Stevanato"))
+
+name = "Rodolfo"
+print(name[0])
+print(name[1])
+print(name[:3])
+print(name[3:])
+
+print("El sobrenombre de Rodolfo es:", name[0:4])
+
+alumnos = ["Rodolfo", "Augusto", "Lorenzo"]
+
+alumnos[0]
+alumnos.append("Ricardo")
+alumnos.remove("Rodolfo")
+
+
