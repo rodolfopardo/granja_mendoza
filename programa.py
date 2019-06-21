1e7a1b0359dae4bc3e153e7659094e2247c7865f
Rodolfo Pardo <rodypardo@gmail.com>
2019-06-21 18:02:52 -0300

    nueva actualización

--- programa.py
+++ programa.py
@@ -8,9 +8,10 @@ Created on Fri Jun 21 17:30:20 2019
 
 # Vamos a declarar los animales que intervienen en la granja
 
-perros = 30 
-gatos = 50
-conejos = 70
+perros = int(input("¿Cuántos perros tiene la granja? = "))
+gatos = int(input("¿Cuántos gatos tiene la granja? = "))
+conejos = int(input("¿Cuántos conejos tiene la granja? = "))
+
 
 total_animales = perros + gatos + conejos
 promedio = total_animales / 3
