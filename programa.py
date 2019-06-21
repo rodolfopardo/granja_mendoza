6d28c09b7dd77db0027d39a19459bfc38731d85b
Rodolfo Pardo <rodypardo@gmail.com>
2019-06-21 17:41:18 -0300

    Python program

---
+++ programa.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 21 17:30:20 2019
+
+@author: rodolfopardo
+"""
+
+# Vamos a declarar los animales que intervienen en la granja
+
+perros = 30 
+gatos = 50
+conejos = 70
+
+total_animales = perros + gatos + conejos
+promedio = total_animales / 3
+
+print("El total de animales de la granja es de", total_animales)
+
+print("El promedio de animales es de", promedio)
+
+
